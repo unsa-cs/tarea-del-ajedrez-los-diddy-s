ba7d445342435071417352304f30c2b14e6fc3e6
MisaelPalomino <misaeljpalominor@gmail.com>
2024-10-03 22:51:39 -0500

    Se libero la memoria reservada de Blackknight

--- ejemplo.c
+++ ejemplo.c
@@ -1,9 +1,11 @@
 #include "chess.h"
 #include "figures.h"
+#include <stdlib.h>
 
 void display() {
   char **blackKnight = reverse(knight);
   char **whitesquare = whiteSquare;
   char **square = superImpose(blackKnight, whitesquare);
+  free(blackKnight);
   interpreter(square);
 }
