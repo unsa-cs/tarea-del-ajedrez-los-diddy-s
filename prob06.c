4445adb63f81031d1ab05b65a1a41767136e23c1
Jesus Alfaro <jalfarob@unsa.edu.pe>
2024-10-05 17:51:25 -0500

    Creamos la funcion special_pieces que retorne las piezas de la primera fila con el codigo del ejercicio anterior

--- prob06.c
+++ prob06.c
@@ -2,7 +2,7 @@
 #include "figures.h"
 #include <stdlib.h>
 
-void display() {
+char ** special_pieces(){
   char **tablero = reverse(repeatH(join(whiteSquare, reverse(whiteSquare)), 4));
   char **torre_caballo = join(rook, knight);
   char **alfin_reyna = join(bishop, queen);
@@ -10,6 +10,12 @@ void display() {
   char **caballo_torre = join(knight, rook);
   char **fil_1 = join(torre_caballo, alfin_reyna);
   char **fil_2 = join(rey_alfil, caballo_torre);
-  char **piezas = reverse(join(fil_1, fil_2));
-  interpreter(superImpose(piezas, tablero));
+  char **piezas = superImpose(join(fil_1, fil_2), tablero);
+  return piezas;
+}
+
+
+void display() {
+  char ** piezas_especiales = special_pieces();
+  interpreter(piezas_especiales);
 }
