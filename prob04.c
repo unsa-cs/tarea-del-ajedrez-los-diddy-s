c59467ffacf868ecc52d40c722c386056efeeed7
Arthur-Meza1 <amezapa@unsa.edu.pe>
2024-10-05 17:13:34 -0500

    Se dibujo el patron de los cuadrados de fondo apartir del 2do ejercicio, en vez de ir creando cuadrado x cuadrado de fondo para cada pieza

--- prob04.c
+++ prob04.c
@@ -3,8 +3,9 @@
 #include <stdlib.h>
 
 void display() {
-  char **torre_b = superImpose(rook, reverse(whiteSquare));
-  char **caballo_b = superImpose(knight, whiteSquare);
+  char **tablero = reverse(repeatH(join(whiteSquare, reverse(whiteSquare)), 4));
+  char **torre_b = rook;
+  char **caballo_b = knight;
 
-  interpreter(join(torre_b, caballo_b));
+  interpreter(superImpose(join(torre_b, caballo_b), tablero));
 }
