9b4571a922dd78d3bddb15cef094f238d6c17af6
ElQuesoHelado <josealvacornejo1@gmail.com>
2024-10-05 18:08:08 -0500

    Se ha decido crear el tableto por separado, esto creando una nueva funcion donde se reutiliza las filas de la funcion special_pieces para dar el intercalado

--- prob06.c
+++ prob06.c
@@ -2,21 +2,28 @@
 #include "figures.h"
 #include <stdlib.h>
 
-char ** special_pieces(){
-  char **tablero = reverse(repeatH(join(whiteSquare, reverse(whiteSquare)), 4));
+char **createTablero() {
+  char **neg = reverse(repeatH(join(whiteSquare, reverse(whiteSquare)), 4)),
+       **blan = repeatH(join(whiteSquare, reverse(whiteSquare)), 4),
+       **estructura = up(neg, blan);
+  return estructura;
+}
+
+char **special_pieces() {
   char **torre_caballo = join(rook, knight);
   char **alfin_reyna = join(bishop, queen);
   char **rey_alfil = join(king, bishop);
   char **caballo_torre = join(knight, rook);
   char **fil_1 = join(torre_caballo, alfin_reyna);
   char **fil_2 = join(rey_alfil, caballo_torre);
-  char **piezas = superImpose(join(fil_1, fil_2), tablero);
-  return piezas;
+  // char **piezas = superImpose(join(fil_1, fil_2), tablero);
+  // return piezas;
+  return join(fil_1, fil_2);
 }
 
-
 void display() {
-  char ** piezas_especiales = special_pieces();
-  char ** fila_peones = repeatH(pawn, 8);
-  interpreter(up(piezas_especiales, fila_peones));
+  char **piezas_especiales = special_pieces();
+  char **fila_peones = repeatH(pawn, 8);
+  // interpreter(up(piezas_especiales, fila_peones));
+  interpreter(createTablero());
 }
