77414d2afe447851c740df57c094a344f1bef2c9
ElQuesoHelado <josealvacornejo1@gmail.com>
2024-10-05 18:30:21 -0500

    Se va a renombro la funcion tablero a crearFilas, ya que nos facilita la creacion del fondo

--- prob06.c
+++ prob06.c
@@ -2,11 +2,11 @@
 #include "figures.h"
 #include <stdlib.h>
 
-char **createTablero() {
+char **createFilas(int filas) {
   char **neg = reverse(repeatH(join(whiteSquare, reverse(whiteSquare)), 4)),
        **blan = repeatH(join(whiteSquare, reverse(whiteSquare)), 4),
        **estructura = up(neg, blan);
-  char **tablero = repeatV(estructura, 4);
+  char **tablero = repeatV(estructura, filas);
   return tablero;
 }
 
@@ -17,14 +17,12 @@ char **special_pieces() {
   char **caballo_torre = join(knight, rook);
   char **fil_1 = join(torre_caballo, alfin_reyna);
   char **fil_2 = join(rey_alfil, caballo_torre);
-  // char **piezas = superImpose(join(fil_1, fil_2), tablero);
-  // return piezas;
+
   return join(fil_1, fil_2);
 }
 
 void display() {
   char **piezas_especiales = special_pieces();
   char **fila_peones = repeatH(pawn, 8);
-  // interpreter(up(piezas_especiales, fila_peones));
-  interpreter(superImpose(king, createTablero()));
+  interpreter(superImpose(up(piezas_especiales, fila_peones), createFilas(2)));
 }
