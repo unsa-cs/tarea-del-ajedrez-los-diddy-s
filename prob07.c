30264d961f6a2c17367d486a079d395ddeffa742
Jesus Alfaro <jalfarob@unsa.edu.pe>
2024-10-06 17:06:03 -0500

    reestructuramos nuestra solucion y ahora vamos a imprimir fila por fila y le pasaremos como argumento la posicion en la que queremos el caballo

--- prob07.c
+++ prob07.c
@@ -2,15 +2,20 @@
 #include "figures.h"
 #include <stdlib.h>
 
-char **createFilas(int filas) {
-  char **neg = reverse(repeatH(join(whiteSquare, reverse(whiteSquare)), 2)),
-       **blan = repeatH(join(whiteSquare, reverse(whiteSquare)), 2),
-       **estructura = up(neg, blan);
-  char **tablero = repeatV(estructura, filas / 2);
-  return tablero;
+char **createFilas(int pos) {
+  char **fila = reverse(whiteSquare);
+  char **caballo = knight;
+  char **cuadrado = whiteSquare;
+  for (int i = 1; i < 8; i++) {
+    if (i % 2 == 0)
+      cuadrado = reverse(whiteSquare);
+    else
+      cuadrado = whiteSquare;
+    if (i == pos)
+      cuadrado = superImpose(caballo, cuadrado);
+    fila = join(fila, cuadrado);
+  }
+  return fila;
 }
 
-void display() {
-
-  interpreter(createFilas(4));
-}
+void display() { interpreter(createFilas(4)); }
