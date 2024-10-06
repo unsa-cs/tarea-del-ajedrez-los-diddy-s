45a5b564b111838a65ea364b12b55165d95ea60d
rja3101 <jorgerivas9725@gmail.com>
2024-10-06 17:33:45 -0500

    Se corrigio el patron del tablero de forma intercalada agregando un nuevo argumento con el cuadrado inicial de la fila

--- prob07.c
+++ prob07.c
@@ -2,15 +2,15 @@
 #include "figures.h"
 #include <stdlib.h>
 
-char **createFilas(int pos) {
-  char **fila = reverse(whiteSquare);
+char **createFilas(int pos, char **cuadrado_in) {
+  char **fila = reverse(cuadrado_in);
   char **caballo = knight;
-  char **cuadrado = whiteSquare;
+  char **cuadrado = cuadrado_in;
   for (int i = 1; i < 8; i++) {
     if (i % 2 == 0)
-      cuadrado = reverse(whiteSquare);
+      cuadrado = reverse(cuadrado_in);
     else
-      cuadrado = whiteSquare;
+      cuadrado = cuadrado_in;
     if (i == pos)
       cuadrado = superImpose(caballo, cuadrado);
     fila = join(fila, cuadrado);
@@ -19,20 +19,20 @@ char **createFilas(int pos) {
 }
 
 void display() {
-  char **fila_1 = createFilas(4);
-  char **fila_2 = createFilas(4);
-  char **fila_3 = createFilas(0);
-  char **fila_4 = createFilas(0);
-  char **fila_5 = createFilas(0);
-  char **fila_6 = createFilas(0);
-  char **fila_7 = createFilas(4);
-  char **fila_8 = createFilas(4);
-  char **tab1 = up(fila_1, fila_2);
-  char **tab2 = up(tab1, fila_3);
-  char **tab3 = up(tab2, fila_4);
-  char **tab4 = up(tab3, fila_5);
-  char **tab5 = up(tab4, fila_6);
-  char **tab6 = up(tab5, fila_7);
-  char **tab7 = up(tab6, fila_8);
+  char **fila_1 = createFilas(4, reverse(whiteSquare));
+  char **fila_2 = createFilas(4, whiteSquare);
+  char **fila_3 = createFilas(0, reverse(whiteSquare));
+  char **fila_4 = createFilas(0, whiteSquare);
+  char **fila_5 = createFilas(0, reverse(whiteSquare));
+  char **fila_6 = createFilas(0, whiteSquare);
+  char **fila_7 = createFilas(4, reverse(whiteSquare));
+  char **fila_8 = createFilas(4, whiteSquare);
+  char **tab1 = up(fila_2, fila_1);
+  char **tab2 = up(fila_3, tab1);
+  char **tab3 = up(fila_4, tab2);
+  char **tab4 = up(fila_5, tab3);
+  char **tab5 = up(fila_6, tab4);
+  char **tab6 = up(fila_7, tab5);
+  char **tab7 = up(fila_8, tab6);
   interpreter(tab7);
 }
