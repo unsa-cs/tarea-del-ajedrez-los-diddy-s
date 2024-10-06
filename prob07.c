3d71f9cf1645e238e9b7963f25c00419dd3d9c66
rja3101 <jorgerivas9725@gmail.com>
2024-10-06 17:19:13 -0500

    Se intento poner los 4 caballos sin embargo el primer patron se repitio para todas las filas perdiendo el intercalado del trablero

--- prob07.c
+++ prob07.c
@@ -18,4 +18,21 @@ char **createFilas(int pos) {
   return fila;
 }
 
-void display() { interpreter(createFilas(4)); }
+void display() {
+  char **fila_1 = createFilas(4);
+  char **fila_2 = createFilas(4);
+  char **fila_3 = createFilas(0);
+  char **fila_4 = createFilas(0);
+  char **fila_5 = createFilas(0);
+  char **fila_6 = createFilas(0);
+  char **fila_7 = createFilas(4);
+  char **fila_8 = createFilas(4);
+  char **tab1 = up(fila_1, fila_2);
+  char **tab2 = up(tab1, fila_3);
+  char **tab3 = up(tab2, fila_4);
+  char **tab4 = up(tab3, fila_5);
+  char **tab5 = up(tab4, fila_6);
+  char **tab6 = up(tab5, fila_7);
+  char **tab7 = up(tab6, fila_8);
+  interpreter(tab7);
+}
